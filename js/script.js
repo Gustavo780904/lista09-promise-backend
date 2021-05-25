7ec20f222baca0bec292e6406775c4aec56eb8f6
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-05-25 17:35:38 -0300

    final

--- js/script.js
+++ js/script.js
@@ -5,21 +5,10 @@ $(document).ready(function() {
     $("#visualizar").click(function() {
         console.log("!!!")
         $("#remove").remove()
-        filter($users)
+        filterTable($users)
         console.log($level)
     })
 })
-
-function table() {
-    $tableRanking = $(`<table></table>`).addClass("");
-    $headerTable = $(`<th>Username</th><th></th>Score<th>Level</th>`)
-
-    $($tableRanking).append($headerTable);
-    $tableLine = new Array();
-    for (data in $ranking) {}
-    $($tableRanking).append($tableLine);
-    $("#view").append($tableRanking);
-}
 
 function readyUsers() {
     fetch("json/users.json")
@@ -37,20 +26,29 @@ function readyUsers() {
         .catch(err => console.log(err))
 }
 
-function filter() {
+function filterTable() {
     $level = getLevel()
+        // if ($level == "easy") {
+        //     $(".mudacor").toogleClass("bg-success")
+
+    // } else if ($level == "medium") {
+    //     $(".mudacor").toogleClass("bg-warning")
+    // } else {
+
+    //     $(".mudacor").toogleClass("bg-danger")
+    // }
+
     strategy = (a, b) => b - a;
     $levelFilter = (userslist, level) => (userslist.filter(valor => valor.level === level)).map((score, user) => ({...user, ...score })).reduce((list, next) => [...list, next], []).sort((a, b) => strategy(a.score, b.score));
     console.log($levelFilter($users, $level))
     $rankingLevel = $levelFilter($users, $level, strategy)
 
-    $tableRanking = $(`<table id="remove"></table>`).addClass("container table");
-    $headerTable = $(`<th>Username</th> <th>Score<th> <th>Level</th>`)
+    $tableRanking = $(`<table id="remove"></table>`).addClass("container table ");
+    $headerTable = $(`<thead><th>Username</th><th>Score</th><th>Level</th></thead>`).addClass("thead-dark")
     $tableLine = new Array();
     for (data in $rankingLevel) {
-        $tableLine[data] = (`<tr><td>${$rankingLevel[data].username}</td><td>${$rankingLevel[data].score}</td><td>${$rankingLevel[data].level}</td></tr>`)
+        $tableLine[data] = (`<tr class="mudacor"><td>${$rankingLevel[data].username}</td><td>${$rankingLevel[data].score}</td><td>${$rankingLevel[data].level}</td></tr>`)
     }
-
     console.log($tableLine)
     $($tableRanking).append($headerTable);
     $($tableRanking).append($tableLine);
